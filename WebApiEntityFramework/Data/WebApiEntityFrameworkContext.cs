3ceae5baa95cda1debf0454ba96a8233aa010a5f
dboscaglia <drboscaglia@gmail.com>
2021-08-31 22:39:20 -0300

    Agregar archivos de proyecto.

---
+++ WebApiEntityFramework/Data/WebApiEntityFrameworkContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiEntityFramework.Models;
+
+namespace WebApiEntityFramework.Data
+{
+    public class WebApiEntityFrameworkContext : DbContext
+    {
+        public WebApiEntityFrameworkContext (DbContextOptions<WebApiEntityFrameworkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiEntityFramework.Models.Libro> Libro { get; set; }
+    }
+}
